06242c7ca66edc22861907200cc0945486b5ddeb
ee5415 <cct5413@gmail.com>
2016-07-15 12:09:34 +0800

    NEW2016_07_15

--- app/src/main/java/com/example/android/justjava/MainActivity.java
+++ app/src/main/java/com/example/android/justjava/MainActivity.java
@@ -2,13 +2,16 @@ package com.example.android.justjava;
 
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.TextView;
+
+import java.text.NumberFormat;
 
 /**
  * This app displays an order form to order coffee.
  */
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,9 +22,33 @@ public class MainActivity extends ActionBarActivity {
     /**
      * This method is called when the order button is clicked.
      */
+    int quantity=0;
     public void submitOrder(View view) {
-        display(1);
+
+        display(quantity);
+        displayPrice(quantity*5);
     }
+
+
+    /*
+    *create increment
+     */
+
+    public void increment(View View)
+    {
+        quantity=quantity+1;
+        display(quantity);
+    }
+    /*
+    *create decrement
+     */
+    public void decrement(View View)
+    {
+        quantity=quantity-1;
+        display(quantity);
+
+    }
+
 
     /**
      * This method displays the given quantity value on the screen.
@@ -31,4 +58,13 @@ public class MainActivity extends ActionBarActivity {
                 R.id.quantity_text_view);
         quantityTextView.setText("" + number);
     }
+
+    /**
+     * This method displays the given price on the screen.
+     */
+    private void displayPrice(int number) {
+        TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
+        priceTextView.setText(NumberFormat.getCurrencyInstance().format(number));
+    }
+
 }
